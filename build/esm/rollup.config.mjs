c836c8455170dfb4d03cc20054f9eea8a2c47154
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-24 13:20:45 +0100

    Clean up rollup.config.mjs header
    Removed copyright notice and TypeScript check comment.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- build/esm/rollup.config.mjs
+++ build/esm/rollup.config.mjs
@@ -1,9 +1,4 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
 
-// @ts-check
 
 import { join, relative } from 'path';
 import { defineConfig } from 'rollup';
