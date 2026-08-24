d9718dee5852e07199311f815af89ab6bc205cb6
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-24 22:58:09 +0100

    Remove copyright notice from lzmaCompressor.ts
    Removed copyright notice from lzmaCompressor.ts
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- website/src/website/utils/lzmaCompressor.ts
+++ website/src/website/utils/lzmaCompressor.ts
@@ -1,7 +1,4 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+
 
 import * as lzma from "lzma/src/lzma_worker";
 import * as msgpack from "messagepack";
