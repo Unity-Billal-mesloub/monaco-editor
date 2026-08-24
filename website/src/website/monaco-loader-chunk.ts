d096a19085622becbeb0b94a612c6f3b667b20b2
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-24 23:03:20 +0100

    Clean up monaco-loader-chunk.ts
    Removed copyright notice and optimization comments from monaco-loader-chunk.ts.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- website/src/website/monaco-loader-chunk.ts
+++ website/src/website/monaco-loader-chunk.ts
@@ -1,10 +1,4 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
 
-// This is an optional load optimization - it inlines the AMD loader and the core editor.
-// We have to configure this before loading editor.main, as it tries to load css
 (global as any).require = {
 	paths: { vs: "node_modules/monaco-editor/min/vs" },
 };
