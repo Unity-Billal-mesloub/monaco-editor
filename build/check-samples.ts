28547590a40f702eefb335b4a58bf939b56960ad
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-24 13:27:22 +0100

    Remove copyright comment from check-samples.ts
    Removed copyright notice from check-samples.ts
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- build/check-samples.ts
+++ build/check-samples.ts
@@ -1,8 +1,4 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
+*/
 import * as fs from 'fs';
 import * as glob from 'glob';
 import * as path from 'path';
